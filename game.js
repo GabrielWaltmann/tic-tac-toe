3ef55e5dfcff4221a71738f7484565b002ab96f7
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-05-12 17:32:54 -0300

    finished

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 let isX = 1
-let mark = ["","","","","","","","","",]
+let mark = ["","","",
+            "","","",
+            "","","",]
+let winner = ""
+let message = document.querySelector("#message")
 
 function turn(){
     if (isX % 2 != 0){
@@ -13,46 +17,90 @@ function turn(){
 }
 
 function win(){
-    if (mark[0] != "" && mark[3] != "" && mark[6] != ""){
+    if( mark[0] == "x" && mark[1] == "x" && mark[2] == "x" ){
+        winner = "x"
+        return true
+    }
+    if( mark[3] == "x" && mark[4] == "x" && mark[5] == "x" ){
+        winner = "x"
+        return true
+    }
+    if( mark[6] == "x" && mark[7] == "x" && mark[8] == "x" ){
+        winner = "x"
         return true
     }
 
-    else if (mark[1] != "" && mark[4] != "" && mark[7] != ""){
+    if( mark[0] == "x" && mark[3] == "x" && mark[6] == "x" ){
+        winner = "x"
         return true
     }
-    
-    else if (mark[2] != "" && mark[5] != "" && mark[8] != ""){
+    if( mark[1] == "x" && mark[4] == "x" && mark[7] == "x" ){
+        winner = "x"
         return true
     }
-
-    else if (mark[0] != "" && mark[1] != "" && mark[2] != ""){
-        return true
-    }
-
-    else if (mark[3] != "" && mark[4] != "" && mark[5] != ""){
+    if( mark[2] == "x" && mark[5] == "x" && mark[8] == "x" ){
+        winner = "x"
         return true
     }
 
-    else if (mark[6] != "" && mark[7] != "" && mark[8] != ""){
+    if( mark[0] == "x" && mark[4] == "x" && mark[8] == "x" ){
+        winner = "x"
+        return true
+    }
+    if( mark[2] == "x" && mark[4] == "x" && mark[6] == "x" ){
+        winner = "x"
         return true
     }
 
-    else if (mark[0] != "" && mark[4] != "" && mark[8] != ""){
+    if( mark[0] == "o" && mark[1] == "o" && mark[2] == "o" ){
+        winner = "o"
+        return true
+    }
+    if( mark[3] == "o" && mark[4] == "o" && mark[5] == "o" ){
+        winner = "o"
+        return true
+    }
+    if( mark[6] == "o" && mark[7] == "o" && mark[8] == "o" ){
+        winner = "o"
         return true
     }
 
-    else if (mark[6] != "" && mark[4] != "" && mark[2] != ""){
+    if( mark[0] == "o" && mark[3] == "o" && mark[6] == "o" ){
+        winner = "o"
         return true
     }
-    else{
-        return false
+    if( mark[1] == "o" && mark[4] == "o" && mark[7] == "o" ){
+        winner = "o"
+        return true
+    }
+    if( mark[2] == "o" && mark[5] == "o" && mark[8] == "o" ){
+        winner = "o"
+        return true
+    }
+
+    if( mark[0] == "o" && mark[4] == "o" && mark[8] == "o" ){
+        winner = "o"
+        return true
+    }
+    if( mark[2] == "o" && mark[4] == "o" && mark[6] == "o" ){
+        winner = "o"
+        return true
     }
 }
 
+
+
 function checked(id){
     let vez
-    if (mark[id] == ""){vez = turn()}    let element = document.getElementById(id)
-    mark[id] = id
+    if (mark[id] == ""){vez = turn()}    
+    let element = document.getElementById(id)
+
+    if (vez == true){
+        mark[id] = "x"
+    }
+    else{
+        mark[id] = "o"
+    }
 
     if (vez == true){
         element.style = 'background: url("https://img.icons8.com/nolan/344/x.png");background-repeat: no-repeat;background-size: cover; background-position: center center; border: 3px solid #00000030'
@@ -61,8 +109,15 @@ function checked(id){
         element.style = 'background: url("https://img.icons8.com/nolan/344/circled.png");background-repeat: no-repeat;background-size: cover; background-position: center center; border: 3px solid #00000030'
     }
 
-    if (win() == true){
-        console.log("GameOver")
+    if (win() == true ){
+        message.style.display = "flex"
+        if (winner = "x"){
+            message.innerHTML = 
+            `<div class="box">
+                <img src="https://img.icons8.com/nolan/344/x.png" alt="">
+                <h2>ganhou!</h2>
+            </div>`
+        }
     }
 
 }
