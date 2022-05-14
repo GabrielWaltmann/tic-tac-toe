d3328694a9adb7e89b2673b190b1bff474284780
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-05-14 17:47:50 -0300

    added loyalt

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# tic-tac-toe
- this is a website that simulates the game  tic tac toe
+<h1> tic-tac-toe </h1>
+ 
+
+<p align="center">this is a website that simulates the game  tic tac toe</p>
+
+<br>
+
+# Technologies
+
+<table align="center">
+  <tr>
+    <td>HTML</td>
+    <td>CSS</td>
+    <td>Javascript</td>
+
+  </tr>
+  <tr>
+    <td>38.6%</td>
+    <td>61.4%</td>
+    <td>61.4%</td>
+
+  </tr>
+</table>
+
+# Fields <!-- Campos -->
++ Dog Name
++ Email
++ Participar
++ Slide show
++ Welcome page
+
+<br>
+
+# Features
+- [X] Email fild
+- [ ] Backend
+- [X] Loyalty
+- [X] Welcome email
+- [X] Animations
+
+<figure>
+  <img align="center" src="readme/readme2.gif"></img>
+</figure>
+
+<figure>
+  <img align="center" src="readme/readme.gif"></img>
+</figure>
+
+<a align="center" href="https://gabrielwaltmann.github.io/dogclub/" target="_blank">
+  <h3 align="center">Test website
+</a>
+
+# Author
+
+<p> Made with by Gabriel Waltmann <a href="https://www.linkedin.com/in/gabriel-waltmann-236114232/">See my Linkedin</a>
--- game.js
+++ game.js
@@ -1,134 +1,162 @@
-let isX = 1
+/* Possibilities */
+let isStar = 1
+
+/* Positions */
 let mark = ["","","",
             "","","",
             "","","",]
+
+/* Winner */
 let winner = ""
+
+/* Result */
 let message = document.querySelector("#message")
 
+/* Check if it's the turn of a star or a diamond */ 
 function turn(){
-    if (isX % 2 != 0){
-        isX++
+    if (isStar % 2 != 0){
+        isStar++
         return true
     }
     else{
-        isX++
+        isStar++
         return false
     }
 }
 
+/* chances of vitory */ 
 function win(){
-    if( mark[0] == "x" && mark[1] == "x" && mark[2] == "x" ){
-        winner = "x"
+/* with Star */ 
+    if( mark[0] == "star" && mark[1] == "star" && mark[2] == "star" ){
+        winner = "star"
         return true
     }
-    if( mark[3] == "x" && mark[4] == "x" && mark[5] == "x" ){
-        winner = "x"
+    if( mark[3] == "star" && mark[4] == "star" && mark[5] == "star" ){
+        winner = "star"
         return true
     }
-    if( mark[6] == "x" && mark[7] == "x" && mark[8] == "x" ){
-        winner = "x"
+    if( mark[6] == "star" && mark[7] == "star" && mark[8] == "star" ){
+        winner = "star"
         return true
     }
-
-    if( mark[0] == "x" && mark[3] == "x" && mark[6] == "x" ){
-        winner = "x"
+    if( mark[0] == "star" && mark[3] == "star" && mark[6] == "star" ){
+        winner = "star"
         return true
     }
-    if( mark[1] == "x" && mark[4] == "x" && mark[7] == "x" ){
-        winner = "x"
+    if( mark[1] == "star" && mark[4] == "star" && mark[7] == "star" ){
+        winner = "star"
         return true
     }
-    if( mark[2] == "x" && mark[5] == "x" && mark[8] == "x" ){
-        winner = "x"
+    if( mark[2] == "star" && mark[5] == "star" && mark[8] == "star" ){
+        winner = "star"
         return true
     }
-
-    if( mark[0] == "x" && mark[4] == "x" && mark[8] == "x" ){
-        winner = "x"
+    if( mark[0] == "star" && mark[4] == "star" && mark[8] == "star" ){
+        winner = "star"
         return true
     }
-    if( mark[2] == "x" && mark[4] == "x" && mark[6] == "x" ){
-        winner = "x"
-        return true
-    }
-
-    if( mark[0] == "o" && mark[1] == "o" && mark[2] == "o" ){
-        winner = "o"
-        return true
-    }
-    if( mark[3] == "o" && mark[4] == "o" && mark[5] == "o" ){
-        winner = "o"
-        return true
-    }
-    if( mark[6] == "o" && mark[7] == "o" && mark[8] == "o" ){
-        winner = "o"
+    if( mark[2] == "star" && mark[4] == "star" && mark[6] == "star" ){
+        winner = "star"
         return true
     }
 
-    if( mark[0] == "o" && mark[3] == "o" && mark[6] == "o" ){
-        winner = "o"
-        return true
+/* with Diamond */ 
+    if( mark[0] == "diamond" && mark[1] == "diamond" && mark[2] == "diamond" ){
+        winner = "diamond"
+        return false
     }
-    if( mark[1] == "o" && mark[4] == "o" && mark[7] == "o" ){
-        winner = "o"
-        return true
+    if( mark[3] == "diamond" && mark[4] == "diamond" && mark[5] == "diamond" ){
+        winner = "diamond"
+        return false
     }
-    if( mark[2] == "o" && mark[5] == "o" && mark[8] == "o" ){
-        winner = "o"
-        return true
+    if( mark[6] == "diamond" && mark[7] == "diamond" && mark[8] == "diamond" ){
+        winner = "diamond"
+        return false
     }
-
-    if( mark[0] == "o" && mark[4] == "o" && mark[8] == "o" ){
-        winner = "o"
-        return true
+    if( mark[0] == "diamond" && mark[3] == "diamond" && mark[6] == "diamond" ){
+        winner = "diamond"
+        return false
     }
-    if( mark[2] == "o" && mark[4] == "o" && mark[6] == "o" ){
-        winner = "o"
-        return true
+    if( mark[1] == "diamond" && mark[4] == "diamond" && mark[7] == "diamond" ){
+        winner = "diamond"
+        return false
+    }
+    if( mark[2] == "diamond" && mark[5] == "diamond" && mark[8] == "diamond" ){
+        winner = "diamond"
+        return false
+    }
+    if( mark[0] == "diamond" && mark[4] == "diamond" && mark[8] == "diamond" ){
+        winner = "diamond"
+        return false
+    }
+    if( mark[2] == "diamond" && mark[4] == "diamond" && mark[6] == "diamond" ){
+        winner = "diamond"
+        return false
     }
 }
 
-function hide(){
-    message.style = "display: none"
-}
-
+/* Box marked */
 function checked(id){
-    let vez
-    if (mark[id] == ""){vez = turn()}    
-    let element = document.getElementById(id)
+    /* Check next player */
+    let nextTime 
 
-    if (vez == true){
-        mark[id] = "x"
+    /* Check next player */
+    if (mark[id] == ""){nextTime = turn()}    
+    let element = document.getElementById(id)
+    if (nextTime == true){
+        mark[id] = "star"
     }
     else{
-        mark[id] = "o"
+        mark[id] = "diamond"
     }
 
-    if (vez == true){
-        element.style = 'background: url("https://img.icons8.com/nolan/344/x.png");background-repeat: no-repeat;background-size: cover; background-position: center center; border: 3px solid #00000030'
-    }
-    if (vez == false){
-        element.style = 'background: url("https://img.icons8.com/nolan/344/circled.png");background-repeat: no-repeat;background-size: cover; background-position: center center; border: 3px solid #00000030'
+    /* if star played */
+    if (nextTime == true){
+        element.style = 
+        `background: url("images/star.png");
+        background-repeat: no-repeat;
+        background-size: 80%;
+        background-position: center center; 
+        border: 3px solid #00000030`
     }
 
+    /* if diamond played */
+    if (nextTime == false){
+        element.style = `background: url("images/diamond.png");
+        background-repeat: no-repeat;
+        background-size: 80%; 
+        background-position: center center; 
+        border: 3px solid #00000030`
+    }
+
+    /* if star won */
     if (win() == true ){
         message.style.display = "flex"
-        if (winner = "x"){
-            message.innerHTML = 
-            `<div class="box">
-                <button onclick="hide()">X</button>
-                <img src="https://img.icons8.com/nolan/344/x.png" alt="">
-                <h2>ganhou!</h2>
-            </div>`
-        }
-        else if (winner = "o"){
-            message.innerHTML = 
-            `<div class="box">
-                <button onclick="hide()">X</button>
-                <img src="https://img.icons8.com/nolan/344/circled.png" alt="">
-                <h2>ganhou!</h2>
-            </div>`
-        }
+        message.innerHTML = 
+        `<div class="box">
+            <h2>Star é o ganhador!</h2>
+            <ion-icon name="refresh-outline" id="reset" onclick="reset()"></ion-icon>
+        </div>`
     }
+    
+    /* if diamond won */
+    else if (win() == false ){
+        message.style.display = "flex"
+        message.innerHTML = 
+        `<div class="box">
 
+            <h2>Diamond é o ganhador!</h2>
+
+            <ion-icon name="refresh-outline" id="reset" onclick="reset()"></ion-icon>
+
+        </div>`
+        
+    }}
+
+/* reset game */
+function reset(){
+    for(i = 0; i<mark.length; i++){
+        mark[i] = ""
+        document.getElementById(i).style = 'background: #07072B; border: none'}
+    message.style.display = "none"
 }
